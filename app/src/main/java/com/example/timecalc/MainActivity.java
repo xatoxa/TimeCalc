097b3e4830c1429b005393402635433265811231
toxic <anton.xatuncev@mail.ru>
2021-10-13 16:00:03 +0300

    The main functionality is implemented.

--- app/src/main/java/com/example/timecalc/MainActivity.java
+++ app/src/main/java/com/example/timecalc/MainActivity.java
@@ -12,6 +12,11 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.time.LocalTime;
+
 import ru.tinkoff.decoro.MaskImpl;
 import ru.tinkoff.decoro.parser.UnderscoreDigitSlotsParser;
 import ru.tinkoff.decoro.slots.PredefinedSlots;
@@ -20,13 +25,13 @@ import ru.tinkoff.decoro.watchers.FormatWatcher;
 import ru.tinkoff.decoro.watchers.MaskFormatWatcher;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText eTxtTime1, eTxtTime2;
+    TextInputLayout txtInpLayt1, txtInpLayt2;
+    TextInputEditText eTxtTime1, eTxtTime2;
     TextView txtViewResult;
     SwitchCompat switchSign;
     ImageButton imgBtnChange;
     Button btnCalculate;
-    boolean sign;
+    boolean can_calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +46,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (switchSign.isChecked()){
-                    sign = true;
                     switchSign.setText(R.string.SwitchPlus);
                 }
                 else {
-                    sign = false;
                     switchSign.setText(R.string.SwitchMinus);
                 }
             }
@@ -61,17 +64,83 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Slot[] slots = new UnderscoreDigitSlotsParser().parseSlots("__:__:__");
-        FormatWatcher formatWatcher = new MaskFormatWatcher( // форматирование текста
-                MaskImpl.createTerminated(slots)
+        Slot[] slots1 = new UnderscoreDigitSlotsParser().parseSlots("__:__:__");
+        FormatWatcher formatWatcher1 = new MaskFormatWatcher( // форматирование текста
+                MaskImpl.createTerminated(slots1)
         );
-        formatWatcher.installOn(eTxtTime1); // устанавливаем форматтер на editText1
-        formatWatcher.installOn(eTxtTime2); // устанавливаем форматтер на editText2
+        formatWatcher1.installOn(eTxtTime1); // устанавливаем форматтер на editText1
 
+        Slot[] slots2 = new UnderscoreDigitSlotsParser().parseSlots("__:__:__");
+        FormatWatcher formatWatcher2 = new MaskFormatWatcher( // форматирование текста
+                MaskImpl.createTerminated(slots2)
+        );
+        formatWatcher2.installOn(eTxtTime2); // устанавливаем форматтер на editText2
 
+        eTxtTime1.addTextChangedListener(new TextValidator(eTxtTime1) {
+            @Override
+            public void validate(TextView textView, String text) {
+                checkTime(text, txtInpLayt1);
+
+            }
+        });
+
+        eTxtTime2.addTextChangedListener(new TextValidator(eTxtTime2) {
+            @Override
+            public void validate(TextView textView, String text) {
+                checkTime(text, txtInpLayt2);
+            }
+        });
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (can_calculate){
+                    LocalTime time1 = LocalTime.parse(eTxtTime1.getText().toString());
+                    String[] strTime2 = eTxtTime2.getText().toString().split(":");
+                    if (switchSign.isChecked()){                                                          //+
+                        LocalTime timeRes = time1.plusHours(Long.parseLong(strTime2[0]));
+                        timeRes = timeRes.plusMinutes(Long.parseLong(strTime2[1]));
+                        timeRes = timeRes.plusSeconds(Long.parseLong(strTime2[2]));
+                        txtViewResult.setText(timeRes.toString());
+                    }
+                    else{                                                               //-
+                        LocalTime timeRes = time1.minusHours(Long.parseLong(strTime2[0]));
+                        timeRes = timeRes.minusMinutes(Long.parseLong(strTime2[1]));
+                        timeRes = timeRes.minusSeconds(Long.parseLong(strTime2[2]));
+                        txtViewResult.setText(timeRes.toString());
+                    }
+                }
+            }
+        });
+    }
+
+    protected void checkTime(String text, TextInputLayout textInputLayout){
+        can_calculate = false;
+        if (text.length() < 8) textInputLayout.setError("Введите полностью.");
+        else {
+            String[] strTime = text.split(":");
+            if (Integer.parseInt(strTime[0]) > 23
+                    && Integer.parseInt(strTime[1]) > 59
+                    && Integer.parseInt(strTime[2]) > 59) textInputLayout.setError("Всё неправильно.");
+            else if (Integer.parseInt(strTime[0]) > 23
+                    && Integer.parseInt(strTime[1]) > 59) textInputLayout.setError("Часы и минуты неправильно");
+            else if (Integer.parseInt(strTime[1]) > 59
+                    && Integer.parseInt(strTime[2]) > 59) textInputLayout.setError("Минуты и секунды неправильно");
+            else if (Integer.parseInt(strTime[0]) > 23
+                    && Integer.parseInt(strTime[2]) > 59)textInputLayout.setError("Часы и секунды неправильно");
+            else if (Integer.parseInt(strTime[0]) > 23) textInputLayout.setError("Часы меньше 24");
+            else if (Integer.parseInt(strTime[1]) > 59) textInputLayout.setError("Минуты меньше 60");
+            else if (Integer.parseInt(strTime[2]) > 59) textInputLayout.setError("Секунды меньше 60");
+            else {
+                textInputLayout.setError(null);
+                can_calculate = true;
+            }
+        }
     }
 
     protected void init(){
+        txtInpLayt1 = findViewById(R.id.eTxtLayoutTime1);
+        txtInpLayt2 = findViewById(R.id.eTxtLayoutTime2);
         eTxtTime1 = findViewById(R.id.eTxtTime1);
         eTxtTime2 = findViewById(R.id.eTxtTime2);
         txtViewResult = findViewById(R.id.txtViewResult);
